5a50da52e4b54084da51e33a934958f4ff76ceb8
Sultan Kirigha <sultan.kirigha@student.moringaschool.com>
2022-07-13 18:15:02 +0300

    created and image banner and nav bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import './App.css';
 import React from 'react';
 import Row from './Row';
 import requests from './requests';
+import Banner from './Banner';
+import Nav from './Nav'
 
 function App() {
   return (
     <div className="App">
-      
+      {/* Nav */}
+      <Nav/>
+
+
+      {/* Banner */}
+      <Banner />
+
+      {/* Rows */}
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
---
+++ src/Nav.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Nav.css"
+
+function Nav() {
+    return (
+        <div className="nav">
+            <img
+            className="nav__logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+            alt="Netflix Logo"
+            />
+
+            <img
+            className="nav__avatar"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            alt="Netflix User Avatar"
+            />
+
+        </div>
+    )
+}
+
+export default Nav
